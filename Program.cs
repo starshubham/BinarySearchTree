f0d309465431aa2efcebaa57712c13695cdb9f29
Shubham Seth <shubhamsth1234@gmail.com>
2021-12-31 23:57:59 +0530

    Added Program.cs file

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree!");
+
+            //UC1
+            BinarySearchTree<int> tree = new BinarySearchTree<int>(56);
+            tree.Insert(30);
+            tree.Insert(70);
+            tree.Display();
+            Console.WriteLine("*********End UC1 Program*********\n");
+
+        }
+    }
+}
